d3b38f7a349042850876ff54fb7d190eebc2d007
Renaud Vitalis <renaud.vitalis@inrae.fr>
2022-02-19 11:52:25 +0100

    Modified src/SimulateCoalescentTree.c

--- src/SimulateCoalescentTree.c
+++ src/SimulateCoalescentTree.c
@@ -63,8 +63,8 @@ void ComputeIdentityProbabilities(Parameters P);
 int Mutation(int allele);
 void WriteOutputs(Parameters P);
 
-struct Tree *deme;
-FILE *outfile;
+struct Tree *population;
+FILE *outputfile;
 
 void SimulateCoalescentTree(int *total_number_of_demes,int *effective_size,double *migration_rate,double *mutation_rate,int *number_of_sampled_demes,int *sample_size,int *number_of_loci) {
 
@@ -74,7 +74,7 @@ void SimulateCoalescentTree(int *total_number_of_demes,int *effective_size,doubl
 	struct Node *mrca;
 	
 	GetRNGstate();	
-	outfile = fopen(OUTPUT, "w");
+	outputfile = fopen(OUTPUT, "w");
 	P.nt = *total_number_of_demes;
 	P.mu = *mutation_rate;
 	P.N = *effective_size;
@@ -82,28 +82,28 @@ void SimulateCoalescentTree(int *total_number_of_demes,int *effective_size,doubl
 	P.nd = *number_of_sampled_demes;
 	P.m = *migration_rate;
 	P.nloci = *number_of_loci;
-	deme = (struct Tree *) malloc (P.nt * sizeof (struct Tree));
+	population = (struct Tree *) malloc (P.nt * sizeof (struct Tree));
 	sampled_lineages = P.n * P.nd;
 	for (i = 0; i < P.nt; ++i) {
-		deme[i].N = P.N;
-		deme[i].m = P.m;
+		population[i].N = P.N;
+		population[i].m = P.m;
 		if (i < P.nd) {
-			deme[i].n = P.n;
+			population[i].n = P.n;
 		} else {
-			deme[i].n = 0;			
+			population[i].n = 0;			
 		}
 	}	
 	for (i = 0; i < P.nloci; ++i) {                                                // Loop over loci
 		max_lineages = 2 * sampled_lineages - 1;
 		for (j = 0; j < P.nt; ++j) {                                           // Initialization of terminal nodes in population 0
-			deme[j].list = (struct Node **) malloc (max_lineages * sizeof (struct Node *));// These are the pointers to the lineages left in the deme
-			deme[j].tree =(struct Node *) malloc(max_lineages * sizeof(struct Node));      // This is the genealogy
+			population[j].list = (struct Node **) malloc (max_lineages * sizeof (struct Node *));// These are the pointers to the lineages left in the population
+			population[j].tree =(struct Node *) malloc(max_lineages * sizeof(struct Node));      // This is the genealogy
 		}
 		for (j = 0; j < P.nt; ++j) {                                           // Initialization of terminal nodes in population 0
 			for (k = 0; k < max_lineages; ++k) {                                           // Initialization of terminal nodes in population 0
-				(deme[j].tree[k]).ancestor = NULL;
-				(deme[j].tree[k]).descendant[0] = NULL;
-				(deme[j].tree[k]).descendant[1] = NULL;
+				(population[j].tree[k]).ancestor = NULL;
+				(population[j].tree[k]).descendant[0] = NULL;
+				(population[j].tree[k]).descendant[1] = NULL;
 			}
 		}
 		mrca = BuildTreeHudson(P);                                                            // Give a sample of genes from a (generation-by-generation) coalescent alogrithm with mutations
@@ -115,20 +115,20 @@ void SimulateCoalescentTree(int *total_number_of_demes,int *effective_size,doubl
 		ComputeIdentityProbabilities(P);
 #endif
 		for (j = 0; j < P.nt; ++j) {                                           // Initialization of terminal nodes in population 0
-			free(deme[j].list);
-			free(deme[j].tree);
+			free(population[j].list);
+			free(population[j].tree);
 		}
 	}
-	free(deme);
-	fclose(outfile);                                                            // Close output files
+	free(population);
+	fclose(outputfile);                                                            // Close output files
 	PutRNGstate();	
 }
 
 struct Node *BuildTreeHudson(Parameters P)
 
 {
-	int *last_node;																				// last_node[i] is the index of the last created node in deme i (former nbr_nodes[i])
-	int *nbr_lineages;																			// nbr_lineages[i] is the number of surviving lineages in deme i
+	int *last_node;																				// last_node[i] is the index of the last created node in population i (former nbr_nodes[i])
+	int *nbr_lineages;																			// nbr_lineages[i] is the number of surviving lineages in population i
 	int total_nbr_lineages = 0;																	// total_nbr_lineages is the total number of survivng lienages across all demes
 	double *p;
 	struct Node *topnode = NULL;
@@ -137,25 +137,25 @@ struct Node *BuildTreeHudson(Parameters P)
 	double time = 0.0;
 	
 	for (i = 0; i < P.nt; ++i) {
-		for (j = 0; j < deme[i].n; ++j) {
-			deme[i].tree[j].time = time;														// tree[j] for j = 0, 1, ..., (n - 1) are the sampled (terminal) nodes
-			deme[i].list[j] = deme[i].tree + j;													// list points to the sampled nodes of deme i
+		for (j = 0; j < population[i].n; ++j) {
+			population[i].tree[j].time = time;														// tree[j] for j = 0, 1, ..., (n - 1) are the sampled (terminal) nodes
+			population[i].list[j] = population[i].tree + j;													// list points to the sampled nodes of population i
 		}
 	}
 	last_node = (int *) malloc(P.nt * sizeof(int));
 	nbr_lineages = (int *) malloc(P.nt * sizeof(int));
 	p = (double *) malloc((2 * P.nt) * sizeof(double));
 	for (i = 0; i < P.nt; ++i) {
-		last_node[i] = nbr_lineages[i] = deme[i].n;   // !!!!!
+		last_node[i] = nbr_lineages[i] = population[i].n;   // !!!!!
 		total_nbr_lineages += nbr_lineages[i];
 	}
 	while (total_nbr_lineages > 1) {
-		p[0] = (double) nbr_lineages[0] * (nbr_lineages[0] - 1) / 2 / deme[0].N;					// Probability of coalescence in deme 0
-		p[1] = (double) p[0] + nbr_lineages[0] * deme[0].m;												// Probability of migration in deme 0
+		p[0] = (double) nbr_lineages[0] * (nbr_lineages[0] - 1) / 2 / population[0].N;					// Probability of coalescence in population 0
+		p[1] = (double) p[0] + nbr_lineages[0] * population[0].m;												// Probability of migration in population 0
 		for (i = 2; i < (2 * P.nt); i += 2) {
 			j = (int) i / 2;																		// Index for the demes
-			p[i] = p[i - 1] + (double) nbr_lineages[j] * (nbr_lineages[j] - 1) / 2 / deme[j].N;	// Probability of coalescence in deme i
-			p[i + 1] = p[i] + (double) nbr_lineages[j] * deme[j].m;										// Probability of migration in deme i
+			p[i] = p[i - 1] + (double) nbr_lineages[j] * (nbr_lineages[j] - 1) / 2 / population[j].N;	// Probability of coalescence in population i
+			p[i + 1] = p[i] + (double) nbr_lineages[j] * population[j].m;										// Probability of migration in population i
 		}
 		sum = p[2 * P.nt - 1];																// Sum over all probabilities
 		for (i = 0; i < (2 * P.nt); ++i) {
@@ -170,42 +170,42 @@ struct Node *BuildTreeHudson(Parameters P)
 		i /= 2;
 		switch (event) {
 			case COALESCENCE : {															// Coalescence in population i
-				number1 = (int) (unif_rand() * nbr_lineages[i]);						// Draw two distinct lineages at random (number1 and number2) in deme[i]
+				number1 = (int) (unif_rand() * nbr_lineages[i]);						// Draw two distinct lineages at random (number1 and number2) in population[i]
 				do {
-					number2 = (int) (unif_rand() * nbr_lineages[i]);						// Draw two distinct lineages at random (number1 and number2) in deme[i]
+					number2 = (int) (unif_rand() * nbr_lineages[i]);						// Draw two distinct lineages at random (number1 and number2) in population[i]
 				}
 				while (number2 == number1);
-				deme[i].tree[last_node[i]].time = time;									// deme[i].tree[last_node[i]] is the next internal node
-				deme[i].list[number1] -> ancestor = deme[i].tree + last_node[i];		// The ancestor of deme[i].tree[number1] is deme[i].tree[last_node[i]] (the internal node now considered)
-				deme[i].list[number2] -> ancestor = deme[i].tree + last_node[i];		// The ancestor of deme[i].tree[number2] is deme[i].tree[last_node[i]] (the internal node now considered)
-				deme[i].tree[last_node[i]].descendant[0] = deme[i].list[number1];		// The descendant_1 of deme[i].tree[last_node[i]] (the internal node now considered) is deme[i].list[number1]
-				deme[i].tree[last_node[i]].descendant[1] = deme[i].list[number2];		// The descendant_2 of deme[i].tree[last_node[i]] (the internal node now considered) is deme[i].list[number2]
-				deme[i].list[number1] = deme[i].tree + last_node[i];					// deme[i].list[number1] now points to the next internal node
-				deme[i].list[number2] = deme[i].list[--nbr_lineages[i]];				// The number of lineages in deme[i] (nbr_lineages[i]) decreases (coalescence) AND deme[i].list[number2] points to the last active lineage in deme[i]
+				population[i].tree[last_node[i]].time = time;									// population[i].tree[last_node[i]] is the next internal node
+				population[i].list[number1] -> ancestor = population[i].tree + last_node[i];		// The ancestor of population[i].tree[number1] is population[i].tree[last_node[i]] (the internal node now considered)
+				population[i].list[number2] -> ancestor = population[i].tree + last_node[i];		// The ancestor of population[i].tree[number2] is population[i].tree[last_node[i]] (the internal node now considered)
+				population[i].tree[last_node[i]].descendant[0] = population[i].list[number1];		// The descendant_1 of population[i].tree[last_node[i]] (the internal node now considered) is population[i].list[number1]
+				population[i].tree[last_node[i]].descendant[1] = population[i].list[number2];		// The descendant_2 of population[i].tree[last_node[i]] (the internal node now considered) is population[i].list[number2]
+				population[i].list[number1] = population[i].tree + last_node[i];					// population[i].list[number1] now points to the next internal node
+				population[i].list[number2] = population[i].list[--nbr_lineages[i]];				// The number of lineages in population[i] (nbr_lineages[i]) decreases (coalescence) AND population[i].list[number2] points to the last active lineage in population[i]
 				--total_nbr_lineages;														// The total number of lineages decreases (coalescence)
 				++last_node[i];																// The index (last_node[i]) of the next internal node to create is increased
 				break;					
 			}
-			case MIGRATION : {																// Migration from deme j in deme i (backward in time)
+			case MIGRATION : {																// Migration from population j in population i (backward in time)
 				do {
 					j = (int) (unif_rand() * P.nt);
 				}
 				while (j == i);
-				number1 = (int) (unif_rand() * nbr_lineages[i]);						// Draw one lineage at random (number1) in deme[i]
-				deme[j].list[nbr_lineages[j]++] = deme[i].list[number1];				//
-				deme[i].list[number1] = deme[i].list[--nbr_lineages[i]];				//
+				number1 = (int) (unif_rand() * nbr_lineages[i]);						// Draw one lineage at random (number1) in population[i]
+				population[j].list[nbr_lineages[j]++] = population[i].list[number1];				//
+				population[i].list[number1] = population[i].list[--nbr_lineages[i]];				//
 				break;
 			}
 		}
 	}
 	for (i = 0; i < P.nt; ++i) {
-		deme[i].nbr_ancestors = nbr_lineages[i];
+		population[i].nbr_ancestors = nbr_lineages[i];
 	}
 	if (total_nbr_lineages == 1) {															    		    	    // If there remains a single lineage (e.g., if this is the mrca of the metapopulation considered...
 		for (i = 0; i < P.nt; ++i) {															    // ... or, more insterestingly, if this is the mrca of the full sample)
-			if (nbr_lineages[i] > 0) break;															    		    // then find the deme where this lineage is
+			if (nbr_lineages[i] > 0) break;															    		    // then find the population where this lineage is
 		}
-		topnode = &deme[i].tree[(last_node[i] - 1)];															// 'topnode' is the address of this last lineage 
+		topnode = &population[i].tree[(last_node[i] - 1)];															// 'topnode' is the address of this last lineage 
 	}
 	free(last_node);
 	free(nbr_lineages);
@@ -254,12 +254,12 @@ void WriteOutputs(Parameters P)
 	
 	for (i = 0; i < P.nd; ++i) {
 		cpt = 0;
-		for (j = 0; j < deme[i].n; ++j) {
-			if (deme[i].tree[j].allele == 0) ++cpt;
+		for (j = 0; j < population[i].n; ++j) {
+			if (population[i].tree[j].allele == 0) ++cpt;
 		}
-		fprintf(outfile,"%8d\t",cpt);
+		fprintf(outputfile,"%8d\t",cpt);
 	}
-	fprintf(outfile,"\n");
+	fprintf(outputfile,"\n");
 }
 
 #ifdef TEST
@@ -273,18 +273,18 @@ void ComputeIdentityProbabilities(Parameters P)
 	npairs[0] = npairs[1] = 0;
 	Q[0] = Q[1] = 0.0;
 	for (i = 0; i < P.nd; ++i) {
-		for (j = 0; j < (deme[i].n - 1); ++j) {
-			for (k = (j + 1); k < deme[i].n; ++k) {
-				if (deme[i].tree[j].allele == deme[i].tree[k].allele) Q[0] += 1;
+		for (j = 0; j < (population[i].n - 1); ++j) {
+			for (k = (j + 1); k < population[i].n; ++k) {
+				if (population[i].tree[j].allele == population[i].tree[k].allele) Q[0] += 1;
 				npairs[0] += 1;
 			}
 		}
 	}
 	for (i = 0; i < (P.nd - 1); ++i) {
 		for (j = (i + 1); j < P.nd; ++j) {
-			for (k = 0; k < deme[i].n; ++k) {
-				for (l = 0; l < deme[j].n; ++l) {
-					if (deme[i].tree[k].allele == deme[j].tree[l].allele) Q[1] += 1;
+			for (k = 0; k < population[i].n; ++k) {
+				for (l = 0; l < population[j].n; ++l) {
+					if (population[i].tree[k].allele == population[j].tree[l].allele) Q[1] += 1;
 					npairs[1] += 1;
 				}
 			}
@@ -293,6 +293,6 @@ void ComputeIdentityProbabilities(Parameters P)
 	for (i = 0; i < 2; ++i) {
 		Q[i] /= (double) npairs[i];
 	}
-	fprintf(outfile,"%10.6f %10.6f\n",Q[0],Q[1]);
+	fprintf(outputfile,"%10.6f %10.6f\n",Q[0],Q[1]);
 }
 #endif
